514f9f9ab75edac253566b11ddb91286a2da1e00
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-25 12:50:26 +0100

    current user route

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,10 +1,17 @@
 import express from "express";
-import { registerUser, loginUser, logoutUser } from "../controllers/auth";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  currentUser,
+} from "../controllers/auth";
+import { requireSignIn } from "../middlewares";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logoutUser);
+router.get("/current-user", requireSignIn, currentUser);
 
 module.exports = router;
