0f83bea3c0e3dbd4efea597350f9b08c9aa65a98
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-25 11:27:53 +0100

    logout route

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/auth";
+import { registerUser, loginUser, logoutUser } from "../controllers/auth";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/logout", logoutUser);
 
 module.exports = router;
