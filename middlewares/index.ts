d43b8f8e9a74cec7fa206165b005111f32f24f11
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-28 13:07:27 +0100

    prepare for deploy

--- middlewares/index.ts
+++ middlewares/index.ts
@@ -4,14 +4,7 @@ import expressJwt, { secretType, RequestHandler, Options } from "express-jwt";
 // gets user information from
 export const requireSignIn: RequestHandler = expressJwt({
   getToken: (req: Request, _: Response) => {
-    console.log(
-      "REQUEST:",
-      req,
-      "COOKIES:",
-      req.cookies,
-      "TOKEN:",
-      req.cookies.token
-    );
+    console.log("COOKIES:", req.cookies, "TOKEN:", req.cookies.token);
     return req.cookies.token;
   },
   secret: process.env.JWT_SECRET as secretType,
